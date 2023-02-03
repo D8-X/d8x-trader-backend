088027cc53aced309bdaf2917f24d1b0975c0153
b1674927 <b1674927@protonmail.com>
2023-02-03 16:18:59 -0500

    cache data via redis

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # d8x-trader-backend
 
+# Prerequisits
+
+- install Redis: https://redis.io/docs/getting-started/installation/install-redis-on-linux/
+
+# Buidl and run backend
+
 npm run build
 npm run start
 http://localhost:3001/
--- src/sdkInterface.ts
+++ src/sdkInterface.ts
@@ -1,17 +1,54 @@
 import { MarketData, PerpetualDataHandler } from "@d8x/perpetuals-sdk";
+import { createClient } from "redis";
 
 export default class SDKInterface {
   private mktData: MarketData | undefined = undefined;
+  private redisClient: ReturnType<typeof createClient>;
+  TIMEOUTSEC = 120;
+
+  constructor() {
+    this.redisClient = createClient();
+  }
 
   public async initialize() {
     const sdkConfig = PerpetualDataHandler.readSDKConfig("testnet");
     this.mktData = new MarketData(sdkConfig);
     await this.mktData.createProxyInstance();
+    await this.initRedis();
     console.log("SDK API initialized");
   }
 
+  private async initRedis() {
+    await this.redisClient.connect();
+    this.redisClient.on("error", (err) => console.log("Redis Client Error", err));
+  }
+
+  private async cacheExchangeInfo() {
+    let tsQuery = Date.now();
+    await this.redisClient.hSet("exchangeInfo", ["ts:query", tsQuery]);
+    let xchInfo = await this.mktData!.exchangeInfo();
+    let info = JSON.stringify(xchInfo);
+    await this.redisClient.hSet("exchangeInfo", ["ts:response", Date.now(), "content", info]);
+    return info;
+  }
+
   public async exchangeInfo(): Promise<string> {
-    let info = await this.mktData!.exchangeInfo();
-    return JSON.stringify(info);
+    let obj = await this.redisClient.hGetAll("exchangeInfo");
+    let info: string = "";
+    //console.log("obj=", obj);
+    if (!Object.prototype.hasOwnProperty.call(obj, "ts:query")) {
+      console.log("first time query");
+      info = await this.cacheExchangeInfo();
+    } else {
+      let timeElapsedS = (Date.now() - parseInt(obj["ts:query"])) / 1000;
+      if (timeElapsedS > this.TIMEOUTSEC) {
+        // reload data through API
+        // no await
+        console.log("re-query exchange info");
+        this.cacheExchangeInfo();
+      }
+      info = obj["content"];
+    }
+    return info;
   }
 }
