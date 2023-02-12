265fd1ced21605402a72c89bf560df7e6b19de61
b1674927 <b1674927@protonmail.com>
2023-02-12 08:56:34 -0500

    loop over rpc

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,18 @@ import NoBroker from "./noBroker";
 import { PerpetualDataHandler, NodeSDKConfig } from "@d8x/perpetuals-sdk";
 
 async function start() {
-  let rpc = JSON.parse(fs.readFileSync("./src/rpc.json", "utf-8"));
-  const sdkConfig: NodeSDKConfig = PerpetualDataHandler.readSDKConfig("testnet");
-  sdkConfig.nodeURL = rpc.RPC[Math.floor(Math.random() * rpc.RPC.length)];
-  console.log(`RPC = ${sdkConfig.nodeURL}`);
-  let d8XBackend = new D8XBrokerBackendApp(new NoBroker(), sdkConfig);
-  await d8XBackend.initialize();
+  while (true) {
+    try {
+      let rpc = JSON.parse(fs.readFileSync("./src/rpc.json", "utf-8"));
+      const sdkConfig: NodeSDKConfig = PerpetualDataHandler.readSDKConfig("testnet");
+      sdkConfig.nodeURL = rpc.RPC[Math.floor(Math.random() * rpc.RPC.length)];
+      console.log(`RPC = ${sdkConfig.nodeURL}`);
+      let d8XBackend = new D8XBrokerBackendApp(new NoBroker(), sdkConfig);
+      await d8XBackend.initialize();
+    } catch (err: any) {
+      console.log("Error in D8XBrokerBackendApp", err);
+      console.log("Restarting with new RPC");
+    }
+  }
 }
 start();
