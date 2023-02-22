bb35c7a0b02890db08fee765d68be3d2123afe1e
b1674927 <b1674927@protonmail.com>
2023-02-21 21:36:02 -0500

    websocket client oracle (inactive)

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,7 @@ export function extractErrorMsg(error: any): string {
   }
   return message;
 }
+
+export function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
