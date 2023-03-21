f1e6a5591e2e76e16db17d1eba73e203b10cc769
b1674927 <b1674927@protonmail.com>
2023-03-21 11:31:10 -0400

    another performance test

--- test/performance.test.ts
+++ test/performance.test.ts
@@ -1,13 +1,23 @@
 async function main() {
-  const baseurl = "https://pyth.testnet.quantena.tech/api/";
+  const baseurl = "https://xc-testnet.pyth.network/api/";
+  let priceIds = [
+    "0xf9c0172ba10dfa4d19088d94f5bf61d3b54d5bd7483a322a982e1373ee8ea31b",
+    "0xd2c2c1f2bba8e0964f9589e060c2ee97f5e19057267ac3284caef3bd50bd2cb5",
+    "0xca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6",
+    "0x41f3625971ca2ed2263e78573fe5ce23e13d2558ed3f2e47ab0f84fb9e7ae722",
+  ];
   const query1 = "latest_price_feeds?ids[]=0xf9c0172ba10dfa4d19088d94f5bf61d3b54d5bd7483a322a982e1373ee8ea31b";
-  const query2 = "latest_vaas_px?ids[]=0xf9c0172ba10dfa4d19088d94f5bf61d3b54d5bd7483a322a982e1373ee8ea31b";
-  const N = 20;
+  const baseQuery2 = "latest_price_feeds?";
+  const N = 100;
   const VAAvsPx = new Array<number>();
   let a1, a2;
   let mu = 0;
   for (let k = 0; k < N; k++) {
     let b = Math.random() < 0.5 ? false : true;
+    let query2 = baseQuery2;
+    for (let j = 0; j < priceIds.length; j++) {
+      query2 = query2 + "ids[]=" + priceIds[j] + "&";
+    }
     if (b == true) {
       a1 = fetch(baseurl + query1);
       a2 = fetch(baseurl + query2);
@@ -19,7 +29,7 @@ async function main() {
     let R1 = await res[0].json();
     let R2 = await res[1].json();
     let t1 = R1[0].price.publish_time;
-    let t2 = R2[0][1].publish_time;
+    let t2 = R2[0].price.publish_time;
     VAAvsPx.push(t2 - t1);
     mu = mu + t2 - t1;
     console.log("diff=", t2 - t1);
