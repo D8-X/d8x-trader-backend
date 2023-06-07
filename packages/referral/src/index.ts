dee57bd2f528692bd156fdabcf05369816ae03b0
b1674927 <b1674927@protonmail.com>
2023-06-07 15:27:56 -0400

    margin token table, prisma migr. migrated to utils

---
+++ packages/referral/src/index.ts
@@ -0,0 +1,14 @@
+import fs from "fs";
+import dotenv from "dotenv";
+import { PerpetualDataHandler, NodeSDKConfig } from "@d8x/perpetuals-sdk";
+
+async function start() {
+  dotenv.config();
+  let configName: string = <string>process.env.SDK_CONFIG_NAME || "";
+  if (configName == "") {
+    throw new Error("Set SDK_CONFIG_NAME in .env (e.g. SDK_CONFIG_NAME=testnet)");
+  }
+  console.log(`loading configuration ${configName}`);
+  const sdkConfig: NodeSDKConfig = PerpetualDataHandler.readSDKConfig(configName);
+}
+start();
