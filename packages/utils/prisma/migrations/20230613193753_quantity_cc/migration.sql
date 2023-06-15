8ce3e353b9deea4373e0b72e109e4c1d13ee7f80
b1674927 <b1674927@protonmail.com>
2023-06-14 20:50:54 -0400

    migrations squashed

--- packages/utils/prisma/migrations/20230613193753_quantity_cc/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `quantity_cc` to the `trades_history` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "trades_history" ADD COLUMN     "quantity_cc" DECIMAL(40,0);
