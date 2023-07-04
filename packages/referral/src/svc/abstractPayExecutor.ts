319e7da2f05560589d90f66ea45083a099797ca4
b1674927 <b1674927@protonmail.com>
2023-07-04 15:09:42 -0400

    remote payment signatures

--- packages/referral/src/svc/abstractPayExecutor.ts
+++ packages/referral/src/svc/abstractPayExecutor.ts
@@ -17,11 +17,26 @@ export default abstract class AbstractPayExecutor {
     return new ethers.Contract(this.multiPayContractAddr, this.ctrMultiPayAbi, signer);
   }
 
+  protected dataReshapeForContract(
+    amounts: bigint[],
+    paymentToAddr: string[]
+  ): { amountsPayable: BigNumber[]; addrPayable: string[] } {
+    let amountsPayable: BigNumber[] = [];
+    let addrPayable: string[] = [];
+    for (let k = 0; k < amounts.length; k++) {
+      // also push zero amounts
+      amountsPayable.push(BigNumber.from(amounts[k].toString()));
+      let addr = paymentToAddr[k] == "" ? ethers.constants.AddressZero : paymentToAddr[k];
+      addrPayable.push(addr);
+    }
+    return { amountsPayable: amountsPayable, addrPayable: addrPayable };
+  }
+
   public abstract transactPayment(
     tokenAddr: string,
     amounts: bigint[],
     paymentToAddr: string[],
-    id: bigint,
+    id: number,
     msg: string
   ): Promise<string>;
 
