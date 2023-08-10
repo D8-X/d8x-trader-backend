dcec852e55ce8a695503cebe903733ce201de2a0
Mantas Jonaitis <j.mantelijo@gmail.com>
2023-08-10 11:43:58 +0300

    referral migrations schema fix, ca certificate

--- packages/referral/src/db/run_migrations.ts
+++ packages/referral/src/db/run_migrations.ts
@@ -3,16 +3,19 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
-  const connStr = process.env.DATABASE_DSN_REFERRAL;
+  const connStr = process.env.DATABASE_DSN_REFERRAL!;
   if (connStr == undefined) {
     Error("run_migrate: database string not defined");
   }
+  const connectionDSNparams = new URL(connStr).searchParams;
+
   try {
     await run({
       migrationsTable: "pgmigrations",
       dir: "./src/migrations",
       direction: "up",
-      databaseUrl: connStr!,
+      schema: connectionDSNparams.has("schema") ? (connectionDSNparams.get("schema") as string) : undefined,
+      databaseUrl: connStr,
     });
 
     console.log("Migrations successfully applied");
