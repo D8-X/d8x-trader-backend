b1ea16b02efed69d40d27c65eef0ea5f0ca99b35
souhailaS <serbout.souhaila@gmail.com>
2026-02-06 15:31:20 +0100

    cleaned up redundancy

--- packages/utils/prisma/migrations/20260206140746_add_leverage/migration.sql
+++ packages/utils/prisma/migrations/20260206140746_add_leverage/migration.sql
@@ -1,5 +1,2 @@
 -- AlterTable
-ALTER TABLE "perpetual_long_id" ALTER COLUMN "valid_to" SET DEFAULT TO_TIMESTAMP(253402300799);
-
--- AlterTable
 ALTER TABLE "trades_history" ADD COLUMN     "leverage" INTEGER;
